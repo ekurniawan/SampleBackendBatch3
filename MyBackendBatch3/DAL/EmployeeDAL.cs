ed6aceee836e658e86040498b234134c06e392b1
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-09 14:52:10 +0700

    menambahkan get employee

---
+++ MyBackendBatch3/DAL/EmployeeDAL.cs
@@ -0,0 +1,50 @@
+﻿using MyBackendBatch3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace MyBackendBatch3.DAL
+{
+    public class EmployeeDAL
+    {
+        private string GetConnStr()
+        {
+            return WebConfigurationManager
+                .ConnectionStrings["MyConnString"].ConnectionString;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            using(SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees order by EmpName asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstEmployee.Add(new Employee
+                        {
+                            EmpId = Convert.ToInt32(dr["EmpId"]),
+                            EmpName = dr["EmpName"].ToString(),
+                            Designation = dr["Designation"].ToString(),
+                            Department = dr["Department"].ToString(),
+                            Qualification = dr["Qualification"].ToString()
+                        });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return lstEmployee;
+        }
+    }
+}
